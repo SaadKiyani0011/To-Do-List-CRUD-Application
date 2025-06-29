9fa770d716c3dd0d93600d1d2fc12f7140740c4a
Saad Zia Kiyani <saadkiyanioffical@gmail.com>
2025-06-29 10:46:18 -0700

    Make The modals of the mem app

--- ToDoList/mem/models.py
+++ ToDoList/mem/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    completed_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
